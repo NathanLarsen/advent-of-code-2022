3afee9798ce1619351d3244752da4398398d79b9
Nathan <larsennathanj@gmail.com>
2022-12-06 23:27:35 -0700

    fix day4

--- day4/main.py
+++ day4/main.py
@@ -1,13 +1,13 @@
 with open("day4/input.txt") as file:
     total = 0
     for line in file:
-        efl1, elf2 = [
+        elf1, elf2 = [
             range(int(y[0]), int(y[1]) + 1)
             for y in [x.split("-") for x in line.strip().split(",")]
         ]
-        # if (efl1.start in elf2 and efl1[-1] in elf2) or (elf2.start in efl1 and elf2[-1] in efl1):
+        # if (elf1.start in elf2 and elf1[-1] in elf2) or (elf2.start in elf1 and elf2[-1] in elf1):
         #     total += 1
-        if (efl1.start in elf2 or efl1[-1] in elf2) or (elf2.start in efl1 or elf2[-1] in efl1):
+        if (elf1.start in elf2 or elf1[-1] in elf2) or (elf2.start in elf1 or elf2[-1] in elf1):
             total += 1
 
 print(total)
